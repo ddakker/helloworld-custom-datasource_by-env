ce4fd0f5fce37cc8a5af80e9748b87397d8a04a0
ddakker <ddakker@gmail.com>
2021-05-10 22:00:27 +0900

    .

--- s2i_artifact/install.sh
+++ s2i_artifact/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 injected_dir=$1
+
+pwd >> /tmp/log.log
+echo ${injected_dir} >> /tmp/log.log
+
 source /usr/local/s2i/install-common.sh
 install_modules ${injected_dir}/modules
 configure_drivers ${injected_dir}/drivers.env
